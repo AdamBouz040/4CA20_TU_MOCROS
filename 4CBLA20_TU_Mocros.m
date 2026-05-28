c5e56041d7b5c7a893ded98af8425ba20962764a
nadirmahnin2007-glitch <nadirmahnin2007@gmail.com>
2026-05-28 12:31:03 +0200

    Add idea for phone number digit recognition
    Added an idea for recognizing phone number digits using frequency and DFT.

--- 4CBLA20_TU_Mocros.m
+++ 4CBLA20_TU_Mocros.m
@@ -5,3 +5,5 @@ fs= 8000
 T= 1/fs 
 signal duration= 100ms 
 Time vector= copy
+Idea; Create a code that recognizes a digits of a phone number based on frequency and dft as a bonus it tells you (based on the 1st 3 digits)
+From which country its from 
